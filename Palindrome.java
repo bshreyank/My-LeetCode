49f407f1e965a9cf50f97e4f4dc426d83ad43a13
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-04-01 18:59:48 +0530

    commit 1

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+public class Palindrome {
+    public static boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int n = s.length();
+
+        for(int i=0;i<n/2;i++){
+            if(s.charAt(i)!=s.charAt(n-i-1))return false;
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        boolean pal = isPalindrome(121);
+        System.out.println(pal);
+    }
+}
