ebba5ed5874b15eed068295ce3a3cb62d9446145
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-08-08 19:28:59 +0530

    EvenNumber-Final

--- JavaScript/evenNumber.js
+++ JavaScript/evenNumber.js
@@ -1,9 +1,10 @@
 
 function isEven(num){
-        for(let i=0;i<=even.length-1;i++){
-                if(i%2==0){
-                        document.write([i])
-                }
+        if(num % 2 ===0){
+                return true;
+        }else{
+                return false;
         }
 }
+
 isEven(2)
