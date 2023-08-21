a49df7c273be494eb15c2b6ff1db80479679337c
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-08-21 19:23:01 +0530

    Sum of Natural Number

---
+++ SumofNaturalNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class SumofNaturalNum{
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter Number:");
+
+                int n = sc.nextInt();
+                int sum = 0;
+
+                for(int i=n; i>0;i/=10){
+                        sum+=i%10;
+                }
+                System.out.println(sum);
+        }
+}
