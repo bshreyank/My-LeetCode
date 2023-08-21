cf8cd9f468c45027b4dd3554f585fe0cae92528d
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-08-21 16:30:24 +0530

    JS-file-changed

--- JavaScript/printDuplicate.js
+++
@@ -1,9 +0,0 @@
-const arr=[1,2,3,1,4,3];
-
-for(let i=0;i<arr.length;i++){
-        for(let j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                        console.log(arr[i]);
-                }
-        }
-}
